e94a0a105475da5d77e8bec5ab72c1ac29927b66
CemCanplt <cem--canpolat@hotmail.com>
2025-01-30 20:15:09 +0300

    Dark Mode'u dövdüm

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,43 @@ import Skills from "./components/Skills";
 import Profile from "./components/Profile";
 import Projects from "./components/Projects";
 import Footer from "./components/Footer";
+import { useEffect } from "react";
+import useLocalStorage from "./hooks/useLocalStorage";
+
+const tarayiciKontrol = () => {
+  // Tema Çekme
+  const prefersDarkScheme = window.matchMedia(
+    "(prefers-color-scheme: dark)"
+  ).matches;
+  const theme = prefersDarkScheme ? "dark" : "light";
+
+  if (theme === "dark") {
+    return true;
+  } else if (theme === "light") {
+    return false;
+  }
+
+  return false;
+};
 
 function App() {
+  const [darkMode, setDarkMode] = useLocalStorage("dark", tarayiciKontrol());
+
+  console.log("TEMA DENEME", darkMode);
+
+  useEffect(() => {
+    // Tarayıcıdan dil çekme
+    const userLang = navigator.language || navigator.userLanguage;
+    console.log("Kullanıcı Dil Tercihi:", userLang);
+
+    // Tema Çekme
+    const prefersDarkScheme = window.matchMedia(
+      "(prefers-color-scheme: dark)"
+    ).matches;
+    const theme = prefersDarkScheme ? "dark" : "light";
+    console.log("Kullanıcı Tema Tercihi:", theme);
+  }, []);
+
   return (
     <MyProvider>
       <Giris />
