1321fc2c5ff033bb874757ede20ee9a84c5a393b
CemCanplt <cem--canpolat@hotmail.com>
2025-02-03 15:44:26 +0300

    Update data.js

--- data.js
+++ data.js
@@ -1,11 +1,93 @@
 export const data = {
   test: "Kontrol Metni",
-  en: {},
-  tr: {
-    giris: {},
-    skills: {},
-    profile: {},
-    projects: {},
-    footer: {},
+  ortak: {
+    giris: {
+      name: {},
+      linkText: { github: "Github", linkedIn: "LinkedIn" },
+      links: { github: "", linkedIn: "" },
+    },
+    skills: {
+      img: {
+        javaScript: "",
+        node: "",
+        react: "",
+        vsCode: "",
+        redux: "",
+        figma: "",
+      },
+      imgText: {
+        javaScript: "JAVASCRIPT",
+        node: "NODE",
+        react: "REACT",
+        vsCode: "VS CODE",
+        redux: "REDUX",
+        figma: "FIGMA",
+      },
+    },
+    profile: { img: "" },
+    projects: {
+      cardOne: { img: "" },
+      cardTwo: { img: "" },
+    },
+    footer: {
+      gmail: "",
+      links: {
+        twitter: "",
+        kare: "",
+        mail: "",
+        instagram: "",
+      },
+    },
   },
+
+  tr: {
+    giris: {
+      dil: {},
+      darkMode: {},
+      title: "",
+      text: "",
+    },
+    skills: {
+      title: "",
+    },
+    profile: {
+      title: "",
+      basics: {
+        basicTitle: "",
+        dogumSoru: "",
+        dogum: "",
+        ikametSoru: "",
+        ikamet: "",
+        egitimDurumuSoru: "",
+        egitimDurumu: "",
+        rolTercihiSoru: "",
+        rolTercihi: "",
+      },
+      aboutMeTitle: "",
+      aboutMeText: "",
+    },
+    projects: {
+      title: "",
+      cardOne: {
+        title: "",
+        text: "",
+        skills: ["react", "redux", "vercel"],
+        site: "",
+        github: "",
+      },
+      cardTwo: {
+        title: "",
+        text: "",
+        skills: ["react", "redux", "vercel"],
+        site: "",
+        github: "",
+      },
+    },
+    footer: {
+      title: "",
+      askMeAnything: "",
+    },
+  },
+
+  en: {},
 };
