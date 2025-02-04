7a975a21466d5170459b6d8ac64ac893ad23bf01
CemCanplt <cem--canpolat@hotmail.com>
2025-02-04 23:14:10 +0300

    projects sonu

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,5 +1,51 @@
+import { useContext } from "react";
+import { MyContext } from "../context/MyProvider";
+
 function Profile() {
-    return ( <></> );
+  const { dilVerisi, ortakVeri } = useContext(MyContext);
+
+  return (
+    <div className="py-12 px-40 flex flex-col bg-(--hafif-mor) gap-5 ">
+      <div>
+        <h1 className="text-5xl font-bold">{dilVerisi?.profile.title}</h1>
+      </div>
+      <div className="flex place-content-between items-start gap-10">
+        <div className="flex flex-col gap-2">
+          <h2 className="text-3xl text-white">
+            {dilVerisi?.profile.basics.basicTitle}
+          </h2>
+          <div className="flex gap-5">
+            <p>{dilVerisi?.profile.basics.dogumSoru}</p>
+            <p className="text-white">{dilVerisi?.profile.basics.dogum}</p>
+          </div>
+
+          <div className="flex gap-5">
+            <p>{dilVerisi?.profile.basics.ikametSoru}</p>
+            <p className="text-white">{dilVerisi?.profile.basics.ikamet}</p>
+          </div>
+
+          <div className="flex gap-5">
+            <p>{dilVerisi?.profile.basics.egitimDurumuSoru}</p>
+            <p className="text-white">
+              {dilVerisi?.profile.basics.egitimDurumu}
+            </p>
+          </div>
+
+          <div className="flex gap-5">
+            <p>{dilVerisi?.profile.basics.rolTercihiSoru}</p>
+            <p className="text-white">{dilVerisi?.profile.basics.rolTercihi}</p>
+          </div>
+        </div>
+        <div>
+          <img src={ortakVeri?.profile.img} alt="profildeki resim" />
+        </div>
+        <div className="flex flex-col w-100 gap-5">
+          <h2 className="text-3xl text-white">{dilVerisi?.profile.aboutMeTitle}</h2>
+          <p className="text-white">{dilVerisi?.profile.aboutMeText}</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Profile;
