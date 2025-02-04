76ebdf4b6a9263dd45a37aea61cc67dfcf2b20c3
CemCanplt <cem--canpolat@hotmail.com>
2025-02-04 23:50:15 +0300

    Proje bölümü

---
+++ src/assets/Proje.jsx
@@ -0,0 +1,46 @@
+function Proje({ card }) {
+  return (
+    <>
+      <div className="bg-white rounded-lg flex gap-10 items-center">
+        <div>
+          <img
+            className="text-[#383838] aspect-square w-250"
+            src={card.img}
+            alt="Soldaki Resim"
+          />
+        </div>
+        <div className="p-4 flex flex-col gap-5">
+          <h2 className="text-(--hafif-mor) text-3xl font-bold">
+            {card.title}
+          </h2>
+
+          <p className="text-[#383838]">{card.text}</p>
+
+          <div className="flex gap-2">
+            {card.skills.map((skill, index) => {
+              return (
+                <p
+                  className="bg-(--hafif-mor) text-white px-2 py-1 rounded-2xl"
+                  key={index}
+                >
+                  {skill}
+                </p>
+              );
+            })}
+          </div>
+
+          <div className="flex gap-5">
+            <a className="text-[#120B39]" href={card.siteLink}>
+              {card.site}
+            </a>
+            <a className="text-[#120B39]" href={card.githubLink}>
+              {card.github}
+            </a>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Proje;
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,5 +1,24 @@
+import Proje from "../assets/Proje";
+import { useContext } from "react";
+import { MyContext } from "../context/MyProvider";
+
 function Projects() {
-  return <></>;
+  const { dilVerisi, ortakVeri } = useContext(MyContext);
+
+  return (
+    <div className="bg-(--acik-yesil) py-12 px-40 flex flex-col gap-10">
+      <div>
+        <h1 className="text-(--hafif-mor) text-5xl font-bold">
+          {dilVerisi?.projects.title}
+        </h1>
+      </div>
+      <div className="flex flex-col gap-5">
+        {dilVerisi?.projects.cards.map((card) => {
+          return <Proje key={card.id} card={card} />;
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default Projects;
