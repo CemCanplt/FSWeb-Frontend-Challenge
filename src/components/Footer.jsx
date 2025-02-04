612edb2a80c3ac0ee06fab331b5d7833d2bc9295
CemCanplt <cem--canpolat@hotmail.com>
2025-02-05 01:55:49 +0300

    Light mode sonu

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,12 +5,29 @@ function Footer() {
   const { dilVerisi, ortakVeri } = useContext(MyContext);
 
   return (
-    <footer className="py-12 px-40 ">
-      <div className="flex flex-col items-center">
-        <h2>{dilVerisi?.footer.title}</h2>
-        <p></p>
-        <p className="underline">{ortakVeri?.footer.gmail}</p>
-        {/* iconlar ve sosyal medya */}
+    <footer className="py-12 px-40 flex place-content-center">
+      <div className="flex flex-col items-center gap-5">
+        <h2 className="text-(--hafif-mor) text-3xl font-bold">
+          {dilVerisi?.footer.title}
+        </h2>
+        <p className="text-black text-center w-72">
+          {dilVerisi?.footer.askMeAnything}
+        </p>
+        <p className="underline text-(--hafif-mor)">
+          {ortakVeri?.footer.gmail}
+        </p>
+        <div className="flex gap-2 place-content-center">
+          {ortakVeri?.footer.links.map((link, index) => {
+            return (
+              <img
+                className="hover:bg-(--acik-yesil) hover:rounded-lg hover:p-1 cursor-pointer"
+                src={link.icon}
+                href={link.iconLink}
+                key={index}
+              />
+            );
+          })}
+        </div>
       </div>
     </footer>
   );
